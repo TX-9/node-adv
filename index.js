ec4f4ff2cc0b6fe7c5ada7e478eb1c7bb5158e0d
TX-9 <jacobleecs@gmail.com>
2020-01-08 13:41:45 -0500

    use pm2

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster');
 // test
 //ab -c 50 -n 500 localhost:3000/fast
 
-// is the file being executed in master mode?
-if (cluster.isMaster) {
-    // cause index.js to be executed again but in child mode
-    cluster.fork(); // every child has its own Thread pool
-    cluster.fork();
-} else {
-    // this is child mode, and acts like a server and do nothing else
-    const express = require('express');
-    const crypto = require('crypto');
-    const app = express();
 
-    app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            res.send('Hi there');
-        });
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send('Hi there');
     });
+});
 
-    app.get('/faster', (req, res) => {
-        res.send('faster');
-    });
+app.get('/faster', (req, res) => {
+    res.send('faster');
+});
 
-    app.listen(3000);
-}
+app.listen(3000);
 
+//pm2 start index.js -i 0
+//0: pm2 decides number of instance
+//pm2 show 0
+//pm2 monitor
+//pm2 delete index
+//pm2 list
